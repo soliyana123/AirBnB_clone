2b25bd4669aa9f2f010913c578cb52888d4000a9
soliye <soliy@gmail.com>
2022-09-02 05:10:16 -0700

    check updated

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,22 @@ import models
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
+    def __init__(self, *args, **kwargs):
+        """ initialization """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    setattr(self, key, type(self))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
    def __str__(self):
         """String representation of the BaseModel class"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
